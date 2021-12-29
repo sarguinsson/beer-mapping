84a350a88314c327ada45ea60d29a52a972786c3
Arnau Sargatal Prat <asargatal90@gmail.com>
2021-12-29 20:54:32 +0100

    Finally making neighbourhoods appear in map!

--- barris.py
+++ barris.py
@@ -1,59 +1,49 @@
 # region imports
 import folium
+import geopandas as gpd
 import pandas as pd
 import pymongo
-import geopandas as gpd
-from shapely.geometry import Point, shape
-from folium import plugins
-
+from pyproj import Transformer
+from shapely.geometry import Point
+import fiona
+from fiona.crs import from_epsg
 # endregion
 
-desired_width = 320
+desired_width = 800
 pd.set_option('display.width', desired_width)
 pd.set_option('display.max_columns', 30)
-pd.set_option('display.max_rows', 400)
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_colwidth', 500)
 
+center = [41.97969558739158, 2.8213967358161915]
 
-input_barris_shapes = "./raw_data/barris/Barris.shp"
-nbh_sbh_df = gpd.read_file(input_barris_shapes)
+# Spatial reference system
+transformer = Transformer.from_crs(4326,25831)
+
+input_bars  = "./raw_data/bars.geojson"
+# input_neighbourhood_shapes = "./raw_data/barris/Barris.shp"
+input_neighbourhood_shapes = "./raw_data/barris.geojson"
+nbh_sbh_df  = gpd.read_file(input_neighbourhood_shapes)
+nbh_sbh_df = nbh_sbh_df.to_crs(epsg=4236)
+# nbh_sbh_df.to_file('./raw_data/barris.geojson', driver='GeoJSON')
+bars_geo_df = gpd.read_file(input_bars, driver='GeoJSON')
+
 print(nbh_sbh_df)
-# nbh_sbh_df = nbh_sbh_df[['BARRIS', 'geometry']]
-# print(nbh_sbh_df)
-# nbh_sbh_df.head()
 
+map_girona = folium.Map(location=center, zoom_start=13, tiles='cartodbpositron')
 
+for _, r in nbh_sbh_df.iterrows():
+    # Without simplifying the representation of each borough,
+    # the map might not be displayed
+    sim_geo = gpd.GeoSeries(r['geometry'])
+    geo_j = sim_geo.to_json()
+    geo_j = folium.GeoJson(data=geo_j,style_function=lambda x: {'fillColor': 'red'})
+    folium.Popup(r['BARRIS']).add_to(geo_j)
+    geo_j.add_to(map_girona)
 
+map_girona.save('index.html')
 
-# nbh_count_df = listing_df.groupby('neighbourhood')['id'].nunique().reset_index()
-# nbh_count_df.rename(columns={'id':'nb'}, inplace=True)
-# nbh_geo_count_df = pd.merge(nbh_geo_df, nbh_count_df, on='neighbourhood')
-# nbh_geo_count_df['QP'] = nbh_geo_count_df['nb'] / nbh_geo_count_df['nb'].sum()
-# nbh_geo_count_df['QP_str'] = nbh_geo_count_df['QP'].apply(lambda x : str(round(x*100, 1)) + '%')
-#
-# from branca.colormap import linear
-# nbh_count_colormap = linear.YlGnBu_09.scale(min(nbh_count_df['nb']),
-#                                             max(nbh_count_df['nb']))
-#
-# nbh_locs_map = folium.Map(location=[48.856614, 2.3522219],
-#                           zoom_start = 12, tiles='cartodbpositron')
-#
-# style_function = lambda x: {
-#     'fillColor': nbh_count_colormap(x['properties']['nb']),
-#     'color': 'black',
-#     'weight': 1.5,
-#     'fillOpacity': 0.7
-# }
-#
-# folium.GeoJson(
-#     nbh_geo_count_df,
-#     style_function=style_function,
-#     tooltip=folium.GeoJsonTooltip(
-#         fields=['neighbourhood', 'nb', 'QP_str'],
-#         aliases=['Neighbourhood', 'Location amount', 'Quote-part'],
-#         localize=True
-#     )
-# ).add_to(nbh_locs_map)
-#
-# nbh_count_colormap.add_to(nbh_locs_map)
-# nbh_count_colormap.caption = 'Airbnb location amount'
-# nbh_count_colormap.add_to(nbh_locs_map)
+# for index, row in bars_geo_df.iterrows():
+#     pt = row['geometry']
+#     is_bar_in_the_nbh = nbh_sbh_df.iloc[[0]]['geometry'].contains(pt)
+#     print(row['name'] + ": {}".format(is_bar_in_the_nbh))
--- helpers.py
+++ helpers.py
@@ -1,4 +1,120 @@
-# locate points inside polygon
-def locate_points_inside_polygon(neighbourhood, points_array):
-    # insert to db if not exists els update
-    return 'something to return'
+import pandas as pd
+import geopandas as gpd
+import folium
+
+from shapely.geometry import Point, shape
+
+def coordinates_to_point(bars):
+    latitutdes = []
+    longitudes = []
+
+    for bar in bars:
+        latitutdes.append(bar['coordinates']['latitude'])
+        longitudes.append(bar['coordinates']['longitude'])
+
+    df = pd.DataFrame({
+        'latitude': latitutdes,
+        'longitude': longitudes
+    })
+
+    bars_points_geometry = [Point(xy) for xy in zip(df.longitude, df.latitude)]
+    crs = {'init': 'epsg:4236'}
+    # Coordinate Reference Systems, "epsg:4326" is a common projection of WGS84 Latitude/Longitude
+    bars_df_with_geometry = gpd.GeoDataFrame(bars, crs=crs, geometry=bars_points_geometry)
+
+    return bars_df_with_geometry
+
+def locate_points_inside_polygon(nbh, bars, nbh_bar_dict):
+    bars_in_nbh_dict_to_add = {}
+    polygon = nbh['geometry']
+
+    for _,bar in bars.iterrows():
+        if (bar['geometry'].within(polygon)):
+            bars_in_nbh_dict_to_add[bar['name']] = nbh['NOM_COMPLE']
+
+    nbh_bar_dict.update(bars_in_nbh_dict_to_add)
+
+def get_neighborhoods_location(nbh_df, bars):
+
+    nbh_bar_dict = {}
+
+    for _, nbh in nbh_df.iterrows():
+        locate_points_inside_polygon(nbh,bars,nbh_bar_dict)
+
+    return nbh_bar_dict
+
+def add_nbh_shapes_to_map(nbh_sbh_df, map, dict):
+    for _, nbh in nbh_sbh_df.iterrows():
+        # Without simplifying the representation of each borough,
+        # the map might not be displayed
+        sim_geo = gpd.GeoSeries(nbh['geometry'])
+        geo_j = sim_geo.to_json()
+        counter = count_values(dict, nbh['NOM_COMPLE'])
+        print(nbh['NOM_COMPLE'],counter)
+        if ( counter > 0):
+            geo_j = folium.GeoJson(data=geo_j, style_function=lambda x: {'fillColor': 'red'})
+        else:
+            geo_j = folium.GeoJson(data=geo_j, style_function=lambda x: {'fillColor': 'blue'})
+        folium.Popup(nbh['NOM_COMPLE']).add_to(geo_j)
+        geo_j.add_to(map)
+
+def add_bar_circlemarker_to_map(map, bars_df):
+    feature_na = folium.FeatureGroup(name='n/a')
+    feature_cheap = folium.FeatureGroup(name='Cheap')
+    feature_normal = folium.FeatureGroup(name='Normal')
+    feature_expensive = folium.FeatureGroup(name='Expensive')
+
+    for _, bar in bars_df.iterrows():
+        popup = """
+        Name : <b>%s</b><br>
+        Beer type : <b>%s</b><br>
+        Price : <b>%.2f  </b><br>
+        """ % (bar['name'], bar.canya['type'], bar.canya['price'])
+        location = tuple([bar['geometry'].y, bar['geometry'].x])
+
+        if bar.canya['price'] <= 1.80 and bar.canya['price'] > 0.00:
+            folium.CircleMarker(
+                location=location,
+                radius=10,
+                tooltip=popup,
+                color='#4C9900',
+                fill_color='#4C9900',
+                fill=True).add_to(feature_cheap)
+        elif bar.canya['price'] >= 2.50:
+            folium.CircleMarker(
+                location=location,
+                radius=10,
+                tooltip=popup,
+                color='#CC0000',
+                fill_color='#CC0000',
+                fill=True).add_to(feature_expensive)
+        elif bar.canya['price'] == 0.00:
+            folium.CircleMarker(
+                location=location,
+                radius=10,
+                tooltip=popup,
+                color='#0066CC',
+                fill_color='#0066CC',
+                fill=True).add_to(feature_na)
+        else:
+            folium.CircleMarker(
+                location=location,
+                radius=10,
+                tooltip=popup,
+                color='#FF8000',
+                fill_color='#FF8000',
+                fill=True).add_to(feature_normal)
+
+    feature_na.add_to(map)
+    feature_cheap.add_to(map)
+    feature_normal.add_to(map)
+    feature_expensive.add_to(map)
+    folium.LayerControl(collapsed=False).add_to(map)
+
+
+def count_values(dict, value):
+    res = 0
+    for key in dict:
+        if dict[key] == value:
+            res = res + 1
+    return res
